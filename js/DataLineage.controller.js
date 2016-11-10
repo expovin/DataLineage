fb2f60326982e865e9149412e3cdc57a2c539e6b
Vincenzo Esposito <expovin@gmail.com>
2016-11-10 18:18:49 +0100

    Calcolo distanza tra oggetti

--- js/DataLineage.controller.js
+++ js/DataLineage.controller.js
@@ -13,8 +13,26 @@ var mainController = ['$scope', myFunc ];
 
 
 function makeElementPosition(boxDimension, Elements){
-	console.log(boxDimension);
-	console.log(Elements);
 
-	return 0;
+	console.log( Elements.qHyperCube.qDataPages[0].qMatrix[0][0].qText);
+	var elements=[];
+	var element={};
+
+	var height = boxDimension.height.substring(0,boxDimension.height.indexOf("px"));
+	var width = boxDimension.width.substring(0,boxDimension.width.indexOf("px"));
+	var hDistance = height / Elements.qHyperCube.qDataPages[0].qMatrix.length;
+
+	var count=0;
+	for(ele in Elements.qHyperCube.qDataPages[0].qMatrix){
+		
+		console.log( Elements.qHyperCube.qDataPages[0].qMatrix[ele][0].qText);
+		element['Name'] = Elements.qHyperCube.qDataPages[0].qMatrix[ele][0].qText;
+		element['hPos'] = count * hDistance;
+		elements.push(element);
+		element={};
+		count +=1;
+	}
+
+	console.log(elements);
+	return elements;
 }
